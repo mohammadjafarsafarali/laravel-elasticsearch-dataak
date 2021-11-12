3c5d425be99a1cc146b60784e0a10dab7ef003b9
mohammad safarali <mjsafarali@gmail.com>
2021-11-12 17:07:08 +0330

    feat: social_username fillable added

--- app/Models/SocialUsername.php
+++ app/Models/SocialUsername.php
@@ -3,9 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
 
 class SocialUsername extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    /**
+     * @var string[]
+     */
+    protected $fillable = [
+        'social_id',
+        'user_id',
+        'username'
+    ];
 }
